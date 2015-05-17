452ed315a5635d4afdd0c3d1de6a278c187aef57
Thomas Zimmermann <bugs@vdm-design.de>
2015-05-17 19:35:03 +0200

    WatchClock: switch to atmega8, implement homeing functionallity

--- WatchClock/main.c
+++ WatchClock/main.c
@@ -32,6 +32,8 @@
 #define CLOCK_5_SEC 10
 #define CLOCK_5_MIN 11
 
+#define BACKUPSTEPS 15
+
 /*
  * Index mapping:
  * pointer 0: pin 1 -> 0, pin 2 -> 1
@@ -39,22 +41,33 @@
  * ...
  * pointer i: pin1 -> i*2, pin 2 -> i*2 + 1
  */
+// FIXME: extend this to 12
+#define N_POINTERS 2
 // FIXME: extend these to 24
-volatile uint8_t *ports[12] = {&PORTB,&PORTB,&PORTB,&PORTB,&PORTB,&PORTB,&PORTB,&PORTB,&PORTD,&PORTD,&PORTD,&PORTD};
-volatile uint8_t *ddrs[12]  = {&DDRB, &DDRB, &DDRB, &DDRB, &DDRB, &DDRB, &DDRB, &DDRB, &DDRD, &DDRD, &DDRD, &DDRD};
-volatile uint8_t pins[12]   = {PB0,   PB1,   PB2,   PB3,   PB4,   PB5,   PB6,   PB7,   PD0,   PD1,   PD2,   PD3};
-volatile uint8_t stepsPerSec[12] = {1,4,1,4,1,4,1,4,1,4,1,4};
-
-volatile uint8_t *port = &PORTB;
-volatile uint8_t *ddr = &DDRB;
+// first seconds pointer then minutes pointer of one clock
+volatile uint8_t *ports[N_POINTERS*2]      = {&PORTD,&PORTD,&PORTD,&PORTD};
+volatile uint8_t *ddrs[N_POINTERS*2]       = {&DDRD, &DDRD, &DDRD, &DDRD};
+const uint8_t pins[N_POINTERS*2]        = {PD0,   PD1,   PD2,   PD3};
+// FIXME: extend this to 6
+// 2 pointers share one ADC
+const uint8_t adcs[N_POINTERS/2]        = {0};
+// FIXME: extend these to 12
+const uint8_t stepsPerSec[N_POINTERS]   = { 1, 4};
+uint8_t position[N_POINTERS]      = { 0, 0};
+const uint8_t zeroSteps[N_POINTERS]     = {12,12};
+const uint8_t stepsAfterZero[N_POINTERS]= { 5, 5};
 
 volatile uint8_t duration = 0;
-volatile uint16_t lastpulse = 0; // bit = 0 -> lastpulse pin 0, bit = 1 -> lastpulse pin 1
-volatile uint16_t topulse = 0;   // bit = 1 -> step this pointer
-volatile uint8_t position[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
+uint16_t lastpulse = 0; // bit = 0 -> lastpulse pin 0, bit = 1 -> lastpulse pin 1
+
+uint8_t seconds = 0;
+uint8_t minutes = 0;
+uint8_t hours = 0;
+uint8_t newtime = 0;
+uint8_t running = 0;
 
 void pulse(uint16_t pointers) {
-	for (uint8_t i = 0; i < 6; i++) { // FIXME: extend to 12
+	for (uint8_t i = 0; i < N_POINTERS; i++) {
  		if (pointers & (1<<i)) {
 			uint8_t index = i*2;
 			uint8_t oindex = index;
@@ -72,21 +85,63 @@ void pulse(uint16_t pointers) {
 		}
 	}
 	duration = 1;
-	TIMSK |= (1<<OCIE0A);       // TC0 compare match A interrupt enable
+	TIMSK |= (1<<OCIE2);       // TC0 compare match A interrupt enable
 }
 
 void init(void) {
-	uint16_t p = 0x0FFF;
+	uint16_t p = 0;
+	uint8_t zerostepcount[N_POINTERS];
+	uint8_t stepsToGo[N_POINTERS];
+	uint8_t stepcount[N_POINTERS];
+	uint8_t backupstepsgone[N_POINTERS/2];
+	uint16_t zeroFound = 0;
+	for (uint8_t i = 0; i < N_POINTERS; i++) {
+		zerostepcount[i] = 0;
+		stepsToGo[i] = stepsAfterZero[i];
+		if (i<N_POINTERS/2) {
+			backupstepsgone[i] = 0;
+		}
+	}
+	uint8_t secondsHomed = 0;
 	while (1) {
-		for (uint8_t i = 0; i < 12; i++) {
-			// check for 0 position
-			// TODO
-			p &= ~(1<<i);
+		p = 0;
+		for (uint8_t i = secondsHomed; i < N_POINTERS; i+=2) {
+			if (secondsHomed == 0 && stepcount[i] > 60) {
+				// move minutes pointer away
+				if (backupstepsgone[i/2] < BACKUPSTEPS) {
+					backupstepsgone[i/2]++;
+					p |= (1<<(i+1));
+				} else {
+					stepcount[i] = 0;
+				}
+			}
+			if (secondsHomed == 1 || stepcount[i] <= 60) {
+				ADMUX = (1<<REFS0) | (1<<REFS1) | (adcs[i/2] & 0x0F);
+				ADCSRA = (1<<ADEN) | (1<<ADSC);
+				while (ADCSRA & (1<<ADSC));
+				// 0.1V*1024/2.56V = 40
+				if (ADC < 40) zerostepcount[i]++;
+				else zerostepcount[i] = 0;
+				// check for 0 position
+				if (zerostepcount[i] >= zeroSteps[i/2]) {
+					zeroFound |= (1<<i);
+				}
+				if (stepsToGo[i] > 0) {
+					p |= (1<<i);
+					if (zeroFound & (1<<i)) stepsToGo[i]--;
+					stepcount[i]++;
+				}
+				ADCSRA = 0;
+			}
 		}
 		if (p > 0) {
 			pulse(p);
+// 			while(duration > 0); // wait for pulse to finish, can go to sleep mode in this time
+			ACSR |= (1<<ACD);
+			MCUCR = (1<<SE);
 		} else {
-			break;
+			if (secondsHomed) break;
+			else secondsHomed = 1;
 		}
 	}
 }
@@ -95,31 +150,40 @@ int main(void) {
 	DDRB = 0; // All inputs
 	PORTB = 0; // All PullUps off
 
-	OCR0A  = (F_CPU/(1024*20))/4;  // number to count up to 50ms/4
-	TCCR0A |= (1<<WGM01);          // Clear Timer on Compare Match (CTC) mode
-	TCCR0B |= (1<<CS00)|(1<<CS02); // clock source CLK/1024
+	// interrupt for motor pulses
+	OCR2  = (F_CPU/(1024*20))/4; // number to count up to 50ms/4
+	TCCR2 = (1<<WGM21);          // Clear Timer on Compare Match (CTC) mode
+	TCCR2 = (1<<CS20)|(1<<CS21)|(1<<CS22); // clock source CLK/1024
+
+	// interrupt every second, for clock
+	TCCR1B = (1<<WGM12) | (1<<CS12) | (1<<CS10);
+	OCR1A   = F_CPU/1024;
+	TIMSK |= ~(1<<OCIE1A);
 
 	sei();
 
 	init();
 
 	while(1) {
-		for (uint8_t i = 0; i < 60; i++) {
-			uint16_t p = 1;//(1<<CLOCK_0_SEC);
-			pulse(p);
-			while(duration > 0); // wait for pulse to finish, can go to sleep mode in this time
+		if (newtime) {
+			running = 1;
+			// set position to reach.
 		}
-		_delay_ms(255);
+		if (running) {
+			uint16_t p;
+			for (uint8_t i = 0; i < 60; i++) {
+				p = (1<<CLOCK_0_SEC);
+			}
+			pulse(p);
+// 			while(duration > 0); // wait for pulse to finish, can go to sleep mode in this time
+		}
+		ACSR |= (1<<ACD);
+		MCUCR = (1<<SE);
+// 		_delay_ms(255);
 	}
 }
 
-#if defined(__AVR_ATtiny2313__) || defined(ATmega168__) || defined(__AVR_ATmega48__) || defined(__AVR_ATmega88__) || defined(__AVR_ATmega640__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega1281__) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega2561__) || defined(__AVR_ATmega324P__) || defined(__AVR_ATmega164P__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644__) || defined(__AVR_ATmega16HVA__) || defined(__AVR_ATtiny2313__) || defined(__AVR_ATtiny48__) || defined(__AVR_ATtiny261__) || defined(__AVR_ATtiny461__) || defined(__AVR_ATtiny861__) || defined(__AVR_AT90USB162__) || defined(__AVR_AT90USB82__) || defined(__AVR_AT90USB1287__) || defined(__AVR_AT90USB1286__) || defined(__AVR_AT90USB647__) || defined(__AVR_AT90USB646) 
-ISR(TIMER0_COMPA_vect) {
-#elif defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny85)
-ISR(TIM0_COMPA_vect ) {
-#else
-ISR(TIMER0_COMP_vect ) {
-#endif
+ISR(TIMER2_COMP_vect ) {
 	cli();
 	++duration;
 	if (duration > 4) {
@@ -127,7 +191,25 @@ ISR(TIMER0_COMP_vect ) {
 		DDRB = 0;
 		PORTB = 0;
 		duration = 0;
-		TIMSK &= ~(1<<OCIE0A);
+		TIMSK &= ~(1<<OCIE2);
+	}
+	sei();
+}
+
+ISR(TIMER1_COMPA_vect ) {
+	cli();
+	seconds++;
+	if (seconds >= 60) {
+		seconds = 0;
+		minutes++;
+		if (minutes >= 60) {
+			minutes = 0;
+			hours++;
+			if (hours >= 24) {
+				hours = 0;
+			}
+		}
+		newtime = 1;
 	}
 	sei();
 }
